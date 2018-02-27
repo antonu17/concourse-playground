6e0166aa590d963e59f28ed355c78c4b2e136f2b
Anton Ustyuzhanin <au@hellofresh.com>
2018-02-27 15:24:53 +0100

    Fix ..

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -4,7 +4,7 @@ PR_ID=$(git config --get pullrequest.id)
 PR_AUTHOR=$(git log -n 1 --pretty=format:'%an')
 TAG="draft."$(git rev-parse HEAD)
 
-printf "**NOT FOR PRODUCTION**\nPR: #${PR_ID}, by ${PR_AUTHOR}\nDeploy with: \`${TAG}\`" > ../artifacts/body
-printf ${TAG} > ../artifacts/hash
+printf "**NOT FOR PRODUCTION**\nPR: #${PR_ID}, by ${PR_AUTHOR}\nDeploy with: \`${TAG}\`" > artifacts/body
+printf ${TAG} > artifacts/hash
 
 tar zcf artifacts/artifact.tgz -C master .
