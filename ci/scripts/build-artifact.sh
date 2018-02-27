b3603c288461f7d740c8018edc566f2f9b5b569d
Anton Ustyuzhanin <au@hellofresh.com>
2018-02-27 15:37:43 +0100

    Update gh-release params

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -3,10 +3,12 @@
 cd master
 PR_ID=$(git config --get pullrequest.id)
 PR_AUTHOR=$(git log -n 1 --pretty=format:'%an')
+COMMITSHA=$(git rev-parse HEAD)
 TAG="draft."$(git rev-parse HEAD)
 
 cd ..
 printf "**NOT FOR PRODUCTION**\nPR: #${PR_ID}, by ${PR_AUTHOR}\nDeploy with: \`${TAG}\`" > artifacts/body
 printf ${TAG} > artifacts/hash
+printf ${COMMITSHA} > artifacts/target_commit
 
 tar zcf artifacts/artifact.tgz -C master .
