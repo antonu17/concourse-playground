bdce7a0df1fb367ef35afc58bcab15bbf2493720
Anton Ustyuzhanin <au@hellofresh.com>
2018-02-27 15:22:15 +0100

    Update draft release message

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+PR_ID=$(git config --get pullrequest.id)
+PR_AUTHOR=$(git log -n 1 --pretty=format:'%an')
+TAG="draft."$(git rev-parse HEAD)
+
+printf "**NOT FOR PRODUCTION**\nPR: #${PR_ID}, by ${PR_AUTHOR}\nDeploy with: \`${TAG}\`" > ../artifacts/body
+printf ${TAG} > ../artifacts/hash
+
 tar zcf artifacts/artifact.tgz -C master .
