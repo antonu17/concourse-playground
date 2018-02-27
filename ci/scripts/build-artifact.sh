27070a7d4454f816192ab7779309c2639ec434fd
Anton Ustyuzhanin <au@hellofresh.com>
2018-02-27 15:30:42 +0100

    Fix message generation

--- ci/scripts/build-artifact.sh
+++ ci/scripts/build-artifact.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+cd master
 PR_ID=$(git config --get pullrequest.id)
 PR_AUTHOR=$(git log -n 1 --pretty=format:'%an')
 TAG="draft."$(git rev-parse HEAD)
 
+cd ..
 printf "**NOT FOR PRODUCTION**\nPR: #${PR_ID}, by ${PR_AUTHOR}\nDeploy with: \`${TAG}\`" > artifacts/body
 printf ${TAG} > artifacts/hash
 
